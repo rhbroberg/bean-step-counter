7b52d1f65fe20142832c749ce331dd7c85ad6889
Richard Broberg <rhb@brobasino.com>
2017-03-20 20:33:18 -0400

    support for writing accelerometer register from scratch area; optional blinking; detect whether accelerometer triggered fall-through of sleep()

--- bean-interrupt-flash.ino
+++ bean-interrupt-flash.ino
@@ -1,5 +1,7 @@
 uint32_t steps;
 uint8_t clearit[2];
+uint8_t override = 0x47;
+uint8_t blink = 0;
 
 void setup() 
 {
@@ -9,15 +11,41 @@ void setup()
   
   Bean.setAccelerationRange(2);
   Bean.enableMotionEvent(ANY_MOTION_EVENT);
-//  Bean.setAccelerometerPowerMode(0x00);
-  Bean.accelRegisterWrite(0x27, 0x0b);
+  Bean.setAccelerometerPowerMode(0x0);
+  Bean.accelRegisterWrite(0x27, 0x00); // 0x0b is default; 0x01 and 0x10 more sensitive
   Bean.accelRegisterWrite(0x28, 0x14);
 }
 
-void loop() {
-  Bean.sleep(0xFFFFffff);
-  steps ++;
+void
+readScratchWriteReg(const uint8_t scratch, const uint8_t reg)
+{
+    ScratchData s = Bean.readScratchData(scratch);
+    if ((s.length > 0) && (s.data[0] != ' '))
+    {
+      // little blink
+      Bean.setLed(255, 0, 0);
+      Bean.sleep(1000);
+      Bean.setLed(0, 0, 0);
+  
+      Bean.accelRegisterWrite(reg, s.data[0]);
+      Bean.setScratchData(scratch, clearit, 2);
+    }
+}
 
+void
+setAccelerometer()
+{
+  if (Bean.getConnectionState() == true)
+  {
+    readScratchWriteReg(3, 0x27);
+    readScratchWriteReg(4, 0x28); 
+    readScratchWriteReg(5, 0x16);
+  }
+}
+
+void
+resetStepCount()
+{
   // if scratchData[2] == 'x', reset count
   ScratchData s = Bean.readScratchData(2);
   if ((s.length > 0) && s.data[0] == 'x')
@@ -28,13 +56,33 @@ void loop() {
     Bean.sleep(5000);
     Bean.setLed(0, 0, 0); 
   }
+}
 
-  // update scratchData[1] with step count
-  Bean.setScratchNumber(1, steps);
+void
+maybeBlink()
+{
+  if (blink)
+  {
+    // little blink
+    Bean.setLed(0, 0, 255);
+    Bean.sleep(10);
+    Bean.setLed(0, 0, 0); 
+  }
+}
 
-  // little blink
-  Bean.setLed(0, 0, 255);
-  Bean.sleep(10);
-  Bean.setLed(0, 0, 0); 
+void
+loop() {
+  Bean.sleep(5000); // 0xFFFFffff);
+
+  if (Bean.checkMotionEvent(ANY_MOTION_EVENT))
+  {
+    steps ++;
+
+    // update scratchData[1] with step count
+    Bean.setScratchNumber(1, steps);
+    maybeBlink();
+  }
+  resetStepCount();
+  setAccelerometer();
 }
 
